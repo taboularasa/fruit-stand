4b44f977177109ae4bf65fde792374b0c6f8d054
David Elliott <david@gophilosophie.com>
2014-03-25 10:24:43 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FruitStand::Application.config.secret_key_base = '9898059ab62adee45d898bdb8ade10c72f4ee1ad1fccad898caa89e75de303323b5022201827ea0d53fd6a0aa4beb461c012d9246c85888fc5f0bcfc0ec5d521'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FruitStand::Application.config.session_store :cookie_store, key: '_fruit_stand_session'
